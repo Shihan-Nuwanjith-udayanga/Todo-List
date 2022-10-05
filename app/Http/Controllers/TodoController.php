4041b053bc9d2e227f1e4d1615a1b36315416963
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-05 15:18:12 +0530

    create done adn delete function

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -30,4 +30,19 @@ class TodoController extends Controller
         return redirect()->back();
         // return redirect()->route('home');
     }
+
+    public function delete($task_id){
+        $task = $this->task->find($task_id);
+        $task->delete();
+
+        return redirect()->back();
+    }
+
+      public function done($task_id){
+        $task = $this->task->find($task_id);
+        $task->done=1;
+        $task->update();
+
+        return redirect()->back();
+    }
 }
