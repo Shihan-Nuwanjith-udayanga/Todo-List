c996fcd0c8f48b06a13472abc6b57fd4e11acb94
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-07 15:51:28 +0530

    add a modal

--- resources/views/pages/todo/index.blade.php
+++ resources/views/pages/todo/index.blade.php
@@ -48,12 +48,35 @@
                                 <a href="{{ route('todo.done', $task->id) }}" class="btn btn-success">
                                     <i class="fas fa-check-circle"></i>
                                 </a>
+                                 <a href="javascript:void(0)" class="btn btn-info">
+                                    <i class="fas fa-pen-square"  data-bs-toggle="modal" data-bs-target="#staticBackdrop"></i>
+                                </a>
                             </td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
 
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <h1>Modal</h1>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Understood</button>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
